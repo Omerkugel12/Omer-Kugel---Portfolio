870b5b5dc46fda2122ddf22b0a2a01e52914e195
Omer Kugel <omerkugel12@gmail.com>
2025-03-02 18:20:14 +0200

    responsive-navbar

--- src/components/NavbarLinks.jsx
+++ src/components/NavbarLinks.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function NavbarLinks() {
-  return <div>NavbarLinks</div>;
+  const links = [
+    { link: "About Me", section: "about" },
+    { link: "Skills", section: "skills" },
+    { link: "Experience", section: "experience" },
+    { link: "Projects", section: "projects" },
+    { link: "Contact", section: "contact" },
+  ];
+
+  return (
+    <ul className="flex gap-6 text-white font-bold text-center py-4 lg:flex-row sm:flex-col lg:relative sm:absolute sm:top-[120%] left-[50%] -translate-x-[50%] lg:text-base sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full sm:py-4">
+      {links.map((link) => {
+        return (
+          <li key={link.section} className="group">
+            <a
+              href={`#${link.section}`}
+              className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+            >
+              {link.link}
+            </a>
+            <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default NavbarLinks;
--- src/components/NavbarLogo.jsx
+++ src/components/NavbarLogo.jsx
@@ -3,8 +3,12 @@ import React from "react";
 function NavbarLogo() {
   return (
     <div>
-      <h1 className="text-white text-2xl font-body">Omer Kugel</h1>
-      <h1 className="text-white text-2xl font-body">OK</h1>
+      <h1 className="text-white text-2xl font-body sm:hidden lg:block">
+        Omer Kugel
+      </h1>
+      <h1 className="text-white text-2xl font-special font-extrabold sm:block lg:hidden">
+        OK
+      </h1>
     </div>
   );
 }
