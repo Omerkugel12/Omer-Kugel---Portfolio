8741091ec0f0341d8634d4e24897c2068bb842b2
Omer Kugel <omerkugel12@gmail.com>
2025-03-04 14:25:36 +0200

    experience-section

--- src/components/experienceSection/ExperienceMain.jsx
+++ src/components/experienceSection/ExperienceMain.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ExperienceTitle from "./ExperienceTitle";
 import ExperienceTop from "./ExperienceTop";
+import AllExperience from "./AllExperience";
 
 function ExperienceMain() {
   return (
-    <main id="experience" className="pt-40 pb-16">
+    <main id="experience" className="pt-40 pb-16 max-w-[1200px] mx-auto">
       <ExperienceTitle />
       <ExperienceTop />
-      <div className="w-full h-1 bg-grey max-w-[1200px] mx-auto items-center sm:hidden xl:block"></div>
+      <div className="w-full h-1 bg-grey max-w-[1200px] mx-auto items-center sm:hidden lg:block"></div>
+      <AllExperience />
     </main>
   );
 }
--- src/components/experienceSection/SingleExperience.jsx
+++ src/components/experienceSection/SingleExperience.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-function SingleExperience() {
-  return <div>SingleExperience</div>;
+function SingleExperience({ role, organization, date, responsibilities }) {
+  return (
+    <div className="lg:w-[240px] lg:h-[400px] sm:w-full border p-4 border-dashed border-orange rounded-2xl font-body">
+      <h3 className="font-bold text-cyan">{role}</h3>
+      <p className="text-orange">{organization}</p>
+      <p className="text-lightGrey">{date}</p>
+
+      <ul className="list-disc pl-4">
+        {responsibilities.map((item, index) => (
+          <li key={index} className="text-white">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SingleExperience;
